a61ad03532500441ce8cd14b2858697ab2e855b8
WRBTS\dsomsanith <Somsanith, Deren dsomsanith@wrberkley.com>
2018-10-15 07:34:44 -0500

    try

--- server.js
+++ server.js
@@ -6,11 +6,12 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 
 if (process.env.NODE_ENV === 'production') {
+  //set static folder
   app.use(express.static('client/build'));
 }
 
-app.get('*', (request, response) => {
-  response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
 // Start the app by listening on the default Heroku port
