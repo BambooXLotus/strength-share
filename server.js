35f75fdfdf0a42fc88e6db5ddf4ecb469bf2986d
bamboo-y50\Bamboo <derensomsanith@gmail.com>
2018-10-15 20:09:46 -0500

    cmon baby

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-//Install express server
-const express = require('express');
 const path = require('path');
-
+const express = require('express');
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/strength-share'));
+// Serve static files
+app.use(express.static(__dirname + '/dist/strength-share'));
 
+// Send all requests to index.html
 app.get('/*', function (req, res) {
-
-  res.sendFile(path.join(__dirname, '/dist/strength-share/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/strength-share/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// default Heroku port
+app.listen(process.env.PORT || 5000);
