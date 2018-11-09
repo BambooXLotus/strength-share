9c70d11a78879dcb96900cf8316312bef2df495f
TECLAST\Deren <derensomsanith@gmail.com>
2018-11-08 21:41:03 -0600

    updated packages and commented out stuff unused

--- src/app/training-plan/training-week/training-day/training-work/training-work-load/training-work-load.component.ts
+++ src/app/training-plan/training-week/training-day/training-work/training-work-load/training-work-load.component.ts
@@ -1,23 +1,23 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+// import { Component, Input, OnInit } from '@angular/core';
+// import { Observable } from 'rxjs';
 
-import { FirebaseService } from './../../../../../firebase.service';
-import { TrainingWorkLoad } from './training-work-load.model';
+// import { FirebaseService } from './../../../../../firebase.service';
+// import { TrainingWorkLoad } from './training-work-load.model';
 
-@Component({
-  selector: 'app-training-work-load',
-  templateUrl: './training-work-load.component.html',
-  styleUrls: ['./training-work-load.component.css']
-})
-export class TrainingWorkLoadComponent implements OnInit {
-  @Input()
-  trainingWorkId: string;
+// @Component({
+//   selector: 'app-training-work-load',
+//   templateUrl: './training-work-load.component.html',
+//   styleUrls: ['./training-work-load.component.css']
+// })
+// export class TrainingWorkLoadComponent implements OnInit {
+//   @Input()
+//   trainingWorkId: string;
 
-  trainingLoad: Observable<TrainingWorkLoad> | null = null;
+//   trainingLoad: Observable<TrainingWorkLoad> | null = null;a
 
-  constructor(private fbService: FirebaseService) {}
+//   constructor(private fbService: FirebaseService) {}
 
-  ngOnInit() {
-    this.trainingLoad = this.fbService.getTrainingWorkLoad(this.trainingWorkId);
-  }
-}
+//   ngOnInit() {
+//     this.trainingLoad = this.fbService.getTrainingWorkLoad(this.trainingWorkId);
+//   }
+// }
